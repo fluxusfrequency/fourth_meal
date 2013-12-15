0eefa0a8ecc9d6ad02aaa369194ac62a49702dfb
Ben Lewis <bennlewis@gmail.com>
2013-12-15 14:26:28 -0700

    put benchmark back into seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 # 2 stockers per restaurant
 # 2 platform administrators
 
-# require 'benchmark'
+require 'benchmark'
 
-# time = Benchmark.measure do 
+time = Benchmark.measure do 
 
   # CITIES
 
@@ -307,7 +307,7 @@
   end
 
 
-# end
+end
 
 puts "Time to seed:"
 puts time
