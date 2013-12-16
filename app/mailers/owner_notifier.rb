01fde932c383c0f450063f5cec0d467545a996a1
Billy <navyosu@gmail.com>
2013-12-15 20:43:40 -0700

    Added method in superman restaurants to send email to owner when approved

--- app/mailers/owner_notifier.rb
+++ app/mailers/owner_notifier.rb
@@ -1,12 +1,12 @@
 class OwnerNotifier < ActionMailer::Base
   default from: "customer_service@noshify.com"
 
-  def owner_email(user, superman, link, restaurant)
-    @user = user
-    @email = superman.email
+  def owner_email(owner, link, restaurant)
+    @owner = owner
+    @email = owner.email
     @link = link
     @restaurant = restaurant
-    mail(to: @email, subject: "New Restaurant #{@restaurant.name} on Noshify Pending Approval!")
+    mail(to: @email, subject: "Your restaurant, #{@restaurant.name}, has been approved!")
   end
 
 end
