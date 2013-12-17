92743c0e7418775d92b1fa3a84205c3b3e80b4ae
Billy <navyosu@gmail.com>
2013-12-17 14:32:03 -0700

    Cleaned up transaction notifier and separated owner emails from user emails - still need to work on content

--- app/mailers/transaction_notifier.rb
+++ app/mailers/transaction_notifier.rb
@@ -3,13 +3,23 @@ class TransactionNotifier < ActionMailer::Base
   default from: "customer_service@noshify.com"
 
   def user_email(email, transaction, link)
+    @customer_name = transaction.order.user.full_name
     @email = email
     @transaction = transaction
-    @address = Address.find(@transaction.address_id)
     @total = order_total(@transaction.order.order_items)
     @link = link
     @restaurant = Restaurant.find(@transaction.order.restaurant_id)
     mail(to: @email, subject: "Order Confirmation for #{@restaurant.name} on Noshify!")
+  end
+
+  def owner_email(email, transaction, link)
+    @customer_name = transaction.order.user.full_name
+    @email = email
+    @transaction = transaction
+    @total = order_total(@transaction.order.order_items)
+    @link = link
+    @restaurant = Restaurant.find(@transaction.order.restaurant_id)
+    mail(to: @email, subject: "Order Received for #{@restaurant.name} on Noshify!")
   end
 
   def order_total(order_items)
