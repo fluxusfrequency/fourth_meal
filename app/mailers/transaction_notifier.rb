cd978e4b3a75a6d4432d4e28607b251f48834c37
Billy <navyosu@gmail.com>
2013-12-17 13:48:56 -0700

    Added resque_mailer gem to Gemfile and deleted stuff that comes with mailers in transaction notifier

--- app/mailers/transaction_notifier.rb
+++ app/mailers/transaction_notifier.rb
@@ -1,12 +1,6 @@
 class TransactionNotifier < ActionMailer::Base
   default from: "customer_service@noshify.com"
 
-  # def welcome_email(user)
-  #   @user = user
-  #   @url = "noshify.herokuapp.com/login"
-  #   mail(to: @user.email, subject: "Welcome to Noshify")
-  # end
-
   def user_email(email, transaction, link)
     @email = email
     @transaction = transaction
