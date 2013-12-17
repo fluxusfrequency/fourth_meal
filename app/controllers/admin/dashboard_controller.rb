4cb4327037913ce65c12ecd9477b38adc80af958
Billy <navyosu@gmail.com>
2013-12-16 18:46:53 -0700

    Removed unused update method in dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -9,22 +9,11 @@ class Admin::DashboardController < ApplicationController
     @locations = Location.all.collect {|location| location.city}.sort
   end
 
-  def update
-    @restaurant = Restaurant.find_by_slug(params[:restaurant_slug])
-    @restaurant.update(restaurant_params)
-  end
-
   def total_sales
     completed_orders = current_restaurant.orders.where(:status => 'complete')
     completed_orders.collect do |order|
       order.total_price
     end.reduce(:+)
-  end
-
-  private
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :description, :slug, :theme, :location)
   end
 
 end
