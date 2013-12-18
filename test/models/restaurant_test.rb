ab02006c97e2d5ad80fdc9c473ba32455f560312
Ben Lewis <bennlewis@gmail.com>
2013-12-18 06:54:54 -0700

    added restaurant model tests to improve coverage

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class RestaurantTest < ActiveSupport::TestCase
-  
+
   test "it is created with valid attributes" do
     assert restaurants(:one).valid?
   end
@@ -41,13 +41,13 @@ class RestaurantTest < ActiveSupport::TestCase
     assert_includes restaurants(:one).orders, orders(:one)
   end
 
-  test "it has restaurant users" do 
+  test "it has restaurant users" do
     assert_includes restaurants(:one).restaurant_users, restaurant_users(:one)
-  end  
+  end
 
-  test "it has a location" do 
+  test "it has a location" do
     assert_equal restaurants(:one).location, locations(:one)
-  end  
+  end
 
   test "the theme is in the set of acceptable themes" do
     restaurant = restaurants(:one)
@@ -56,6 +56,33 @@ class RestaurantTest < ActiveSupport::TestCase
     assert_equal "dark", restaurant.theme
     restaurant.update(theme: "naranja")
     refute restaurant.valid?
+  end
+
+  test "it can generate its slug" do
+    refute_equal "long-john-silvers", restaurants(:six).name
+    assert_nil restaurants(:six).slug
+    restaurants(:six).generate_slug
+    assert_equal "long-john-silver-s", restaurants(:six).slug
+  end
+
+  test "it can find one of its owners" do
+    assert_equal "bird", restaurants(:three).find_owner.display_name
+  end
+
+  test "it can activate, deactivate, and toggle itself" do
+    refute restaurants(:four).active?
+    restaurants(:four).activate
+    assert restaurants(:four).active?
+    restaurants(:four).deactivate
+    refute restaurants(:four).active?
+    restaurants(:four).toggle_status
+    assert restaurants(:four).active?
+    restaurants(:four).toggle_status
+    refute restaurants(:four).active?
+  end
+
+  test "it knows if it is pending" do
+    assert restaurants(:three).pending?
   end
 
 end
