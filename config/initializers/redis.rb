78ac7d9c7f12197daba6b5d88d7fd858839d7bf6
Ben Lewis <bennlewis@gmail.com>
2013-12-18 11:45:09 -0700

    changed redis config file to parse uri

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-$resque_redis_url = uris_per_environment[rails_env]
-Resque.redis = $resque_redis_url
+$resque_config = YAML.load_file(rails_root + '/config/resque.yml')
+uri = URI.parse($resque_config[rails_env])
+Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
