c30f95dbf06f4ee3ef75cf64406926cfafe2c9fd
Adam89 <Adamm1@aston.ac.uk>
2013-12-18 19:11:13 -0700

    moved the tests to relevant file

--- test/integration/restaurant_admin_test.rb
+++ test/integration/restaurant_admin_test.rb
@@ -57,6 +57,95 @@ class RestaurantAdminTest < Capybara::Rails::TestCase
     @admin.destroy
     @ru.destroy
   end
+  
+   def test_user_can_create_a_restaurant
+
+    visit root_path
+
+    click_on "Sign up or Log in"
+
+    within "#new_user" do
+      fill_in "Email", with: 'benji@example.com'
+      fill_in "Full name", with: 'Benjamin Franklin'
+      fill_in "Display name", with: 'benitobeans'
+      fill_in "Password", with: 'password'
+      fill_in "Password confirmation", with: 'password'
+      click_button "Create User"
+    end
+    assert_content page, 'Logged in'
+    click_on "Create New Restaurant"
+
+    assert_content page, "Creating A New Restaurant"
+
+    fill_in "Name", with: "Pizza Hut"
+    fill_in "Description", with: "Best Pizza House"
+    fill_in "Slug", with: "pizza-hut"
+    click_on "Create Restaurant"
+
+    assert current_path == root_path, "redirected back to the restaurant root path"
+    assert_content page, "Your request has been submitted. You will be emailed when your restaurant is approved."
+  end
+
+  def test_pending_restaurant_cannot_be_seen
+    visit restaurant_root_path(restaurants(:four))
+    assert_content page, "Sorry, we couldn't find the restaurant you requested in our sytem."
+  end
+
+
+  def test_approved_restaurant_can_be_seen_when_active
+    visit root_path
+    visit restaurant_root_path(restaurants(:seven))
+    assert_content page, "Sorry, this restaurant is currently offline for maintenance."
+  end
+
+  def test_owner_can_activate_approved_restaurant
+
+    @admin = User.create(full_name: "Joan of Arc", display_name: "Joan A.", email: 'jarc@thestake.fr', password: 'martyr', password_confirmation: 'martyr')
+    @ru = RestaurantUser.create(user: @admin, restaurant: restaurants(:seven), role: "owner")
+
+    # restaurant owner logs in
+    visit root_path
+    click_on "Sign up or Log in"
+    within "#login-form" do
+      fill_in "Email", with: 'jarc@thestake.fr'
+      fill_in "Password", with: 'martyr'
+      click_button "Log In"
+    end
+
+    assert_content page, "Logged in"
+    visit admin_path(restaurants(:seven))
+    refute_content page, "You're not authorized to do that!"
+    assert_content page, "Manage Your Restaurant"
+
+    assert_content page, "Offline"
+    click_on "Activate"
+    assert_content page, "Active"
+  end
+
+  def test_pending_restaurant_cannot_be_activated_by_restaurant_owner
+    @admin = User.create(full_name: "Joan of Arc", display_name: "Joan A.", email: 'jarc@thestake.fr', password: 'martyr', password_confirmation: 'martyr')
+    @ru = RestaurantUser.create(user: @admin, restaurant: restaurants(:four), role: "owner")
+
+    #make sure restaurant is pending
+    assert_equal "pending", restaurants(:four).status
+
+    # restaurant owner logs in
+    visit root_path
+    click_on "Sign up or Log in"
+    within "#login-form" do
+      fill_in "Email", with: 'jarc@thestake.fr'
+      fill_in "Password", with: 'martyr'
+      click_button "Log In"
+    end
+
+    assert_content page, "Logged in"
+
+    visit admin_path(restaurants(:four))
+
+    refute_content page, "Manage Your Restaurant"
+
+  end
+
 
 end
 
